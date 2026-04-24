3d668c429ea9f9120c93fd3ff7e88b07c5e3167f
Mohamed1995Ahmed <engmohamedabdelhakem@gmail.com>
2026-04-24 22:53:48 +0300

    Add controllers, models, and views for departments and employees
    Created `DepartmentController` and `EmployeeController` for managing departments and employees, including actions for listing and creating entries. Updated `AppDBContextModelSnapshot.cs` for table mappings. Modified `Department` and `Employee` models to allow nullable references. Added Razor views for creating and displaying departments and employees. Updated the database connection string in `appsettings.json` to reflect a new SQL Server instance.

--- WebApplication2/Models/Models/Department.cs
+++ WebApplication2/Models/Models/Department.cs
@@ -5,9 +5,7 @@
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string Description { get; set; }
-		public ICollection<Employee> Employees
-		{
-			get;
-		}
+		public ICollection<Employee>? Employees {  get; set; }
+		
 	}
 }
